f9c803f5d47597b7cb0e1a691accb3c559f1d070
yethranayeh <58709123+yethranayeh@users.noreply.github.com>
2024-10-04 20:02:01 +0300

    add horizontal movement logic to Ship

--- src/components/Ship.tsx
+++ src/components/Ship.tsx
@@ -1,10 +1,28 @@
 import { AnimatedSprite, Sprite, Container } from "@pixi/react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Resource, Texture } from "pixi.js";
 import { config } from "../config";
 
+const shipSize = 48;
+
 export const Ship = () => {
 	const [frames, setFrames] = useState<Array<Texture<Resource>>>([]);
+	const [x, setX] = useState(config.canvas.width / 2);
+
+	const keyListener = useCallback(
+		(e: KeyboardEvent) => {
+			if (e.key === "a") {
+				if (x - shipSize >= 0) {
+					setX(x - shipSize);
+				}
+			} else if (e.key === "d") {
+				if (x + shipSize <= config.canvas.width) {
+					setX(x + shipSize);
+				}
+			}
+		},
+		[x, setX]
+	);
 
 	useEffect(() => {
 		const frameTextures = [0, 1, 2].map((n) => Texture.from(`/ship/engine/idle-${n}.png`));
@@ -12,8 +30,16 @@ export const Ship = () => {
 		setFrames(frameTextures);
 	}, []);
 
+	useEffect(() => {
+		document.addEventListener("keydown", keyListener);
+
+		return () => {
+			document.removeEventListener("keydown", keyListener);
+		};
+	}, [x, keyListener]);
+
 	return (
-		<Container interactive sortableChildren x={config.canvas.width / 2} y={750} onclick={(e) => console.log(e)}>
+		<Container sortableChildren x={x} y={750}>
 			<Sprite image='/ship/base.png' zIndex={3} anchor={0.5} />
 			<Sprite image='/ship/engine.png' zIndex={2} anchor={0.5} />
 			{!!frames.length && (
