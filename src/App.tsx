b7774486f180eec84877971433a869a75823becc
yethranayeh <58709123+yethranayeh@users.noreply.github.com>
2024-10-06 23:17:22 +0300

    add start screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Stage } from "@pixi/react";
+import { Stage, Text } from "@pixi/react";
 import { useAtom } from "jotai";
 
 import { config } from "./config";
@@ -11,15 +11,14 @@ import { GameOver } from "./components/ui/text/GameOver";
 import { UI } from "./components/ui/UI";
 import { EndingScene } from "./EndingScene";
 
-const App = () => {
+import { TextStyle } from "pixi.js";
+import { PlayButton } from "./components/ui/buttons/PlayButton";
+
+function Game() {
 	const [isGameOver] = useAtom(gameAtom.isOver);
 	const [isFinished] = useAtom(gameAtom.isFinished);
-	const [session] = useAtom(gameAtom.session);
-
 	return (
-		<Stage key={session} width={config.canvas.width} height={config.canvas.height} options={{ background: 0x1099bb }}>
-			<Background />
-
+		<>
 			{isFinished ? (
 				<EndingScene />
 			) : (
@@ -35,6 +34,50 @@ const App = () => {
 					<RestartButton />
 				</>
 			)}
+		</>
+	);
+}
+
+const style = new TextStyle({
+	fontFamily: "monospace",
+	fill: 0xffffff,
+	fontSize: 18,
+	fontWeight: "400",
+	wordWrap: true,
+	wordWrapWidth: config.canvas.width - 24
+});
+
+function StartScreen() {
+	return (
+		<>
+			<Text
+				anchor={0.5}
+				x={config.canvas.width / 2}
+				y={config.canvas.height / 2}
+				style={style}
+				text={`In the distant future, the Earth has become uninhabitable, and humanity is on the brink of extinction.
+
+			The last remaining survivors have boarded a small but advanced spaceship, The Glider, on a desperate mission to reach a distant, habitable planet known only as Elysium.
+
+			This planet, though distant and shrouded in mystery, is the only hope for humanity's survival.
+
+			However, the journey is perilous. The ship must navigate through a treacherous asteroid belt before reaching safety.`}
+			/>
+
+			<PlayButton />
+		</>
+	);
+}
+
+const App = () => {
+	const [isGameStarted] = useAtom(gameAtom.isStarted);
+	const [session] = useAtom(gameAtom.session);
+
+	return (
+		<Stage key={session} width={config.canvas.width} height={config.canvas.height} options={{ background: 0x1099bb }}>
+			<Background />
+
+			{isGameStarted ? <Game /> : <StartScreen />}
 		</Stage>
 	);
 };
---
+++ src/components/ui/buttons/PlayButton.tsx
@@ -0,0 +1,33 @@
+import { Sprite } from "@pixi/react";
+import { useState } from "react";
+import { useAtom } from "jotai";
+
+import { gameAtom } from "../../../atoms/game.atom";
+import { config } from "../../../config";
+
+export function PlayButton() {
+	const setGameStarted = useAtom(gameAtom.isStarted)[1];
+	const [isHovering, setIsHovering] = useState(false);
+	const [isPressed, setIsPressed] = useState(false);
+
+	return (
+		<Sprite
+			eventMode='static'
+			onclick={() => {
+				setGameStarted(true);
+			}}
+			onmouseenter={() => setIsHovering(true)}
+			onmouseleave={() => {
+				setIsHovering(false);
+				setIsPressed(false);
+			}}
+			onmousedown={() => setIsPressed(true)}
+			onmouseup={() => setIsPressed(false)} // FIXME: this does not get triggered when button is pressed and mouse is moved away from button and released
+			image={`/ui/buttons/play/${isPressed ? "pressed" : isHovering ? "hover" : "base"}.png`}
+			x={config.canvas.width / 2}
+			y={650}
+			anchor={0.5}
+			scale={1.5}
+		/>
+	);
+}
